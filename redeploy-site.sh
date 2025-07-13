df16da0f2b22d5e478bb0d1a2cfd736daff0b805
ebukaamadiobi <ebukaamadiobi@gmail.com>
2025-07-13 16:07:16 +0100

    Update redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-tmux kill-session
-cd /home/mlh-portfolio/
+cd /root/mlh-portfolio/
 git fetch && git reset origin/main --hard
 source venv/bin/activate
 pip install -r requirements.txt
-tmux new-session -d "flask run --host=0.0.0.0"
+systemctl daemon-reload
+systemctl restart myportfolio
